741186284720a0b56229b8fd9a956c6e8f6c8416
Luiz Paulo <luizpaulocs@gmail.com>
2025-01-19 08:21:04 -0300

    added testimonial's pic

--- php/index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<section class="section-hero">
-  <div class="hero">
-    <h1>Embrace your <span>beauty and refine</span> your style</h1>
-  </div>
-</section>
-
-<?php get_footer(); ?>
